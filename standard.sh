fd82bfbd26f40f77ec271143da2e125d334d026e
Karl Douglas Cahill <70593626+KarlDouglas@users.noreply.github.com>
2022-12-06 14:55:42 +0100

    Update standard.sh

--- standard.sh
+++ standard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo pigpiod
-pigs 5 100
-pigs 6 100
-pigs 19 100
-pigs 26 100
+pigs pfs 5 100
+pigs pfs 6 100
+pigs pfs 19 100
+pigs pfs 26 100
 pigs p 5 251
 pigs p 6 247
 pigs p 19 230
